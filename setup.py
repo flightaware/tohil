e9f282935e191763d67f8e87bc15518864da4bc5
Karl Lehenbauer <karl@flightaware.com>
2021-03-18 16:57:08 +0000

    start renaming to tohil

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup, Extension
 
 # dammit beavis library names are tcl86 on freebsd on tcl8.6 on mac
 
-module1 = Extension('tclpy',
-                    define_macros = [('PACKAGE_VERSION', '"0.5.0"')],
+module1 = Extension('tohil',
+                    define_macros = [('PACKAGE_VERSION', '"1.0.0"')],
                     libraries = ['tcl86'],
                     include_dirs = ['/usr/local/include', '/usr/local/include/tcl8.6'],
                     library_dirs = ['/usr/local/lib'],
-                    sources = ['generic/tclpy.c'])
+                    sources = ['generic/tohil.c'])
 
-setup (name = 'tclpy',
-       version = '0.5.0',
+setup (name = 'tohil',
+       version = '1.0.0',
        description = 'python-tcl integration',
        ext_modules = [module1])
