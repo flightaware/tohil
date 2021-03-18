0c508739b2aca7fe60063bd6d6e9798b695ee627
Karl Lehenbauer <karl@flightaware.com>
2021-03-18 04:28:59 +0000

    python side builds and installs on freebsd

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup, Extension
 
+# dammit beavis library names are tcl86 on freebsd on tcl8.6 on mac
+
 module1 = Extension('tclpy',
                     define_macros = [('PACKAGE_VERSION', '"0.5.0"')],
-                    libraries = ['tcl8.6'],
+                    libraries = ['tcl86'],
+                    include_dirs = ['/usr/local/include', '/usr/local/include/tcl8.6'],
                     library_dirs = ['/usr/local/lib'],
                     sources = ['generic/tclpy.c'])
 
