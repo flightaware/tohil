98f427d8415db9741fb8dac022b07e28caf9aa83
Karl Lehenbauer <karl@flightaware.com>
2021-03-19 03:30:59 +0000

    nascent rivet support

--- pysrc/tohil/__init__.py
+++ pysrc/tohil/__init__.py
@@ -1,8 +1,27 @@
 """ tohil """
 
+import sys
+
 from tohil._tohil import (
     eval, expr, getvar, interp, setvar, subst, call,
 )
 
 from tohil.tcller import TclWriter
 
+class RivetControl:
+    def __init__(self):
+        self.activated = 0
+        self.tclWriter = TclWriter()
+
+    def activate(self):
+        if self.activated:
+            return
+
+        sys.stdout = self.tclWriter
+        self.activated = 1
+
+global rivetControl
+rivetControl = RivetControl()
+
+def rivet():
+    rivetControl.activate()
