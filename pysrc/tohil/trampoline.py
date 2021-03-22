1f6721aa066a14c9917bbdb8c4b691547a1dc548
Karl Lehenbauer <karl@flightaware.com>
2021-03-22 16:57:42 +0000

    remove cruft

--- pysrc/tohil/trampoline.py
+++ pysrc/tohil/trampoline.py
@@ -52,35 +52,3 @@ class Trampoline:
             pass
         return 'success'
 
-    def get_exception(self):
-        exc_type, exc_value, exc_tb = sys.exc_info()
-        if exc_type is None:
-            raise RuntimeError('get_exception called with no extant exception')
-        exc_list = traceback.format_exception(exc_type, exc_value, exc_tb)
-        err_msg = exc_list.pop().rstrip()
-        exc_list.reverse()
-        exc_list.pop() # get rid of header line
-        print(f'exc_type {exc_type}, exc_value {exc_value}, exc_tb {exc_tb}')
-        return exc_list
-
-    def playpen(self, command):
-        try:
-            exec(command, self.globals, self.locals)
-        except Exception as err:
-            exc_type, exc_value, exc_tb = sys.exc_info()
-            exc_list = traceback.format_exception(exc_type, exc_value, exc_tb)
-            print("---")
-            print(exc_list)
-            print("---")
-            if 'Traceback' in exc_list[0]:
-                exc_list.pop(0)
-                new_list = list()
-            for string in exc_list:
-                if self.carat_pattern.search(string) is not None:
-                    new_list[0], string = string, new_list[0]
-                new_list.insert(0, string)
-            return new_list, exc_type, exc_value, exc_tb
-        finally:
-            pass
-        return
-
