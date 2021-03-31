5b083a84ab5b88d0f1bae4b56f80bd4180ae3c68
Karl Lehenbauer <karl@flightaware.com>
2021-03-31 21:39:59 +0000

    correct unit test for new behavior of "run"

--- tests/test_run.py
+++ tests/test_run.py
@@ -7,7 +7,7 @@ import tohil
 class TestRun(unittest.TestCase):
     def test_run1(self):
         """exercise tohil.run"""
-        self.assertEqual(tohil.run("print('icky')"), "icky\n")
+        self.assertEqual(tohil.run("print('icky')"), "icky")
 
 if __name__ == "__main__":
     unittest.main()
