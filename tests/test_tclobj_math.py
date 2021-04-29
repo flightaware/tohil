036896d417a74a6466ac29079f04503125e8dbe8
Karl Lehenbauer <karl-gh@lehenbauer.com>
2021-04-29 10:21:05 -0500

    recode about half these tests to use hypothesis.  incomplete.

--- tests/test_tclobj_math.py
+++ tests/test_tclobj_math.py
@@ -1,168 +1,201 @@
+
+import hypothesis
+from hypothesis import given, assume, strategies as st
 import unittest
 
 import tohil
 
-
 class TestTclObj(unittest.TestCase):
-    def test_tclobj_math1(self):
-        """exercise tohil.tclobj plus math ops"""
-        t = tohil.tclobj(5)
+    @given(st.integers(-40000, 40000), st.integers(-40000, 40000))
+    def test_tclobj_math1(self, i, j):
+        """exercise tohil.tclobj 'plus' math ops"""
+        t = tohil.tclobj(i)
 
-        self.assertEqual(t + 4, 9)
-        self.assertEqual(t + t, 10)
-        self.assertEqual(6 + t, 11)
+        assert(t + j == i + j)
+        assert(t + t == i + i)
+        assert(6 + t == 6 + i)
 
-        self.assertEqual(t + 4., 9.)
-        self.assertEqual(t + float(t), 10.)
-        self.assertEqual(6. + t, 11.)
+        assert(t + 4. == i + 4.)
+        assert(4. + t == i + 4.)
+        assert(t + float(t) == i + float(i))
+        assert(6. + t == 6. + i)
 
-    def test_tclobj_math2(self):
-        """exercise tohil.tclobj plus math ops"""
-        t = tohil.tclobj('5')
+    @given(st.integers(-40000, 40000), st.integers(-40000, 40000))
+    def test_tclobj_math2(self, i, j):
+        """exercise tohil.tclobj 'minus' math ops"""
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        self.assertEqual(t - 3, 2)
-        self.assertEqual(t - t, 0)
-        self.assertEqual(6 - t, 1)
+        assert(ti - 3 == i - 3)
+        assert(ti - ti == 0)
+        assert(6 - ti == 6 - i)
 
-        self.assertEqual(t - 4., 1.)
-        self.assertEqual(t - float(t), 0.)
-        self.assertEqual(6. - t, 1.)
+        assert(tj - 4. == j - 4.)
+        assert(tj - float(tj) == 0.)
+        assert(6. - ti == 6 - i)
 
-    def test_tclobj_math3(self):
+        assert(tj - ti == j - i)
+
+    @given(st.floats(-40000.0, 40000.0), st.integers(-40000, 40000))
+    def test_tclobj_math3(self, f, i):
         """exercise tohil.tclobj plus float math ops"""
-        t = tohil.tclobj('5.0')
+        t = tohil.tclobj(f)
 
-        self.assertEqual(t - 3, 2.0)
-        self.assertEqual(t - t, 0.0)
-        self.assertEqual(6 - t, 1.0)
+        assert(t == f)
+        assert(t - i == f - i)
+        assert(t - t == 0.0)
+        assert(6 - t == 6 - f)
 
-        self.assertEqual(t - 4., 1.)
-        self.assertEqual(t - float(t), 0.)
-        self.assertEqual(6. - t, 1.)
+        assert(i - t == i - f)
+        assert(int(t) - i == int(t) - i)
+        assert(6. - t == 6. - f)
 
-    def test_tclobj_math4(self):
+    @given(st.integers(-40000.0, 40000.0), st.integers(-40000, 40000))
+    def test_tclobj_math4(self, i, j):
         """exercise tohil.tclobj multiply math ops"""
-        t = tohil.tclobj('6')
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        self.assertEqual(t * 3, 18)
-        self.assertEqual(t * t, 36)
-        self.assertEqual(7 * t, 42)
-        self.assertEqual(t * 7, 42)
+        assert(ti * 3 == i * 3)
+        assert(ti * j == i * j)
+        assert(j * ti == i * j)
+        assert(tj * ti == i * j)
+        assert(7 * ti == 7 * i)
+        assert(ti * 7 == 7 * i)
+        assert(ti * ti == i * i)
 
-        self.assertEqual(t * 4., 24.)
-        self.assertEqual(t * float(t), 36.)
-        self.assertEqual(t * t, 36.)
-        self.assertEqual(8. * t, 48.)
+        assert(ti * 4. == i * 4.)
+        assert(ti * float(ti) == i * float(i))
+        assert(8. * ti == 8. * i)
 
-    def test_tclobj_math5(self):
+    @given(st.floats(-40000.0, 40000.0), st.floats(-40000, 40000))
+    def test_tclobj_math5(self, u, v):
         """exercise tohil.tclobj multiply float math ops"""
-        t = tohil.tclobj('6.')
+        t6 = tohil.tclobj('6.')
+        tu = tohil.tclobj(u)
+        tv = tohil.tclobj(v)
 
-        self.assertEqual(t * 3, 18.)
-        self.assertEqual(t * t, 36.)
-        self.assertEqual(7 * t, 42.)
+        assert(tu * 6 == u * 6)
+        assert(tu * 6. == u * 6.)
+        assert(tu * t6== u * 6.)
+        assert(tu * tu == u * u)
 
-        self.assertEqual(t * 4., 24.)
-        self.assertEqual(t * t, 36.)
-        self.assertEqual(-6. * t, -36.)
+        assert(tu * tv == u * v)
 
-    def test_tclobj_math6(self):
+    @given(st.integers(-40000, 40000), st.integers(-40000, 40000))
+    def test_tclobj_math6(self, i, j):
         """exercise tohil.tclobj remainder ops"""
-        t = tohil.tclobj(5)
+        assume(i != 0 and j != 0)
 
-        self.assertEqual(t % 7, 5 % 7)
-        self.assertEqual(11 % t, 11 % 5)
-        self.assertEqual(-7 % t, -7 % 5)
-        self.assertEqual(t % -7, 5 % -7)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        tm7 = tohil.tclobj('-7.')
+        assert(ti % 7 == i % 7)
+        assert(ti % j == i % j)
+        assert(11 % ti == 11 % i)
+        assert(-7 % ti == -7 % i)
+        assert(ti % -7 == i % -7)
 
-        self.assertEqual(t % -7., 5 % -7.)
-        self.assertEqual(t % tm7, 5 % -7.)
-        self.assertEqual(t % t, 5 % 5)
-        self.assertEqual(tm7 % t, -7 % 5)
+        assert(ti % tj == i % j)
+        assert(ti % j == i % j)
+        assert(i % tj == i % j)
 
-    def test_tclobj_math7(self):
+    @given(st.integers(0, 1024), st.integers(0, 23))
+    def test_tclobj_math7(self, i, j):
         """exercise tohil.tclobj left shift math ops"""
-        t = tohil.tclobj(2)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        self.assertEqual(t << 4, 32)
-        self.assertEqual(t << t, 8)
-        self.assertEqual(16 << 2, 64)
+        assert(ti << j == i << j)
+        assert(ti << tj == i << j)
+        assert(i << tj == i << j)
 
-    def test_tclobj_math8(self):
+        assert(ti << 4 == i << 4)
+        assert(4 << tj == 4 << j)
+
+    @given(st.integers(0, 2000000000), st.integers(0, 23))
+    def test_tclobj_math8(self, i, j):
         """exercise tohil.tclobj right shift math ops"""
-        t = tohil.tclobj(8)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        self.assertEqual(t >> 1, 4)
-        self.assertEqual(t >> t, 0)
-        self.assertEqual(65536 >> t, 256)
+        assert(ti >> j == i >> j)
+        assert(ti >> tj == i >> j)
+        assert(i >> tj == i >> j)
 
-    def test_tclobj_math9(self):
+    @given(st.integers(0, 2000000000), st.integers(0, 2000000000))
+    def test_tclobj_math9(self, i, j):
         """exercise tohil.tclobj "and" math ops"""
-        t = tohil.tclobj(31)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        self.assertEqual(t & 16, 16)
-        self.assertEqual(t & t, 31)
-        self.assertEqual(8 & t, 8)
+        assert(ti & j == i & j)
+        assert(ti & tj == i & j)
+        assert(i & tj == i & j)
 
-    def test_tclobj_math10(self):
+    @given(st.integers(0, 2000000000), st.integers(0, 2000000000))
+    def test_tclobj_math10(self, i j):
         """exercise tohil.tclobj "or" math ops"""
-        t = tohil.tclobj(16)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        self.assertEqual(t | 8, 24)
-        self.assertEqual(4 | t, 20)
-        self.assertEqual(t | t, 16)
+        assert(ti | j, i | j)
+        assert(ti | tj, i | j)
+        assert(i | tj, i | j)
 
-    def test_tclobj_math11(self):
+    @given(st.integers(0, 2000000000), st.integers(0, 2000000000))
+    def test_tclobj_math11(self, i j):
         """exercise tohil.tclobj "xor" math ops"""
-        t = tohil.tclobj(31)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        self.assertEqual(t ^ 15, 16)
-        self.assertEqual(15 ^ t, 16)
-        self.assertEqual(t ^ t, 0)
+        assert(ti ^ j == i ^ j)
+        assert(ti ^ tj == i ^ j)
+        assert(i ^ tj == i ^ j)
 
-    def test_tclobj_math12(self):
+    @given(st.integers(0, 2000000000), st.integers(0, 2000000000))
+    def test_tclobj_math12(self, i, j):
         """exercise tohil.tclobj "inplace add" math ops"""
-        t = tohil.tclobj(7)
-        t5 = tohil.tclobj(5)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        t += 5
-        self.assertEqual(t, 12)
-        t += t
-        self.assertEqual(t, 24)
-        t += t5
-        self.assertEqual(t, 29)
-        t5 += t
-        self.assertEqual(t5, 34)
+        ti += j
+        assert(ti == i + j)
 
+        ti = tohil.tclobj(i)
+        ti += ti
+        assert(ti == i + i)
+
+        ti = tohil.tclobj(i)
+        ti += tj
+        assert(ti == i + j)
+
+    @given(st.integers(0, 2000000000), st.integers(0, 2000000000))
     def test_tclobj_math13(self):
         """exercise tohil.tclobj "inplace subtract" math ops"""
-        t = tohil.tclobj(7)
-        t5 = tohil.tclobj(5)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        t -= -1
-        self.assertEqual(t, 8)
-        t -= t
-        self.assertEqual(t, 0)
-        t -= t5
-        self.assertEqual(t, -5)
-        t5 -= t
-        self.assertEqual(t5, 10)
+        ti -= j
+        assert(ti == i - j)
 
-    def test_tclobj_math14(self):
+        ti = tohil.tclobj(i)
+        ti -= tj
+        assert(ti == i - j)
+
+    @given(st.integers(-40000, 40000), st.integers(-40000, 40000))
+    def test_tclobj_math14(self, i j):
         """exercise tohil.tclobj "inplace multiply" math ops"""
-        t = tohil.tclobj(7)
-        t5 = tohil.tclobj(5)
+        ti = tohil.tclobj(i)
+        tj = tohil.tclobj(j)
 
-        t *= -1
-        self.assertEqual(t, -7)
-        t *= t
-        self.assertEqual(t, 49)
-        t *= t5
-        self.assertEqual(t, 245)
-        t5 *= t
-        self.assertEqual(t5, 1225)
+        ti *= j
+        assert(ti == i * j)
+
+        ti = tohil.tclobj(i)
+        ti *= tj
+        assert(ti == i * j)
+
 
     def test_tclobj_math15(self):
         """exercise tohil.tclobj "inplace left shift" math ops"""
@@ -170,9 +203,9 @@ class TestTclObj(unittest.TestCase):
         t5 = tohil.tclobj(2)
 
         t <<= 2
-        self.assertEqual(t, 28)
+        assert(t, 28)
         t5 <<= t5
-        self.assertEqual(t5, 8)
+        assert(t5, 8)
 
     def test_tclobj_math16(self):
         """exercise tohil.tclobj "inplace right shift" math ops"""
@@ -180,9 +213,9 @@ class TestTclObj(unittest.TestCase):
         t5 = tohil.tclobj(2)
 
         t >>= 1
-        self.assertEqual(t, 16)
+        assert(t, 16)
         t >>= t5
-        self.assertEqual(t, 4)
+        assert(t, 4)
 
     def test_tclobj_math16(self):
         """exercise tohil.tclobj "inplace bitwise or" math ops"""
@@ -190,9 +223,9 @@ class TestTclObj(unittest.TestCase):
         t2 = tohil.tclobj(8)
 
         t |= 32
-        self.assertEqual(t, 48)
+        assert(t, 48)
         t |= t2
-        self.assertEqual(t, 56)
+        assert(t, 56)
 
     def test_tclobj_math17(self):
         """exercise tohil.tclobj "inplace bitwise and" math ops"""
@@ -200,38 +233,38 @@ class TestTclObj(unittest.TestCase):
         t2 = tohil.tclobj(8)
 
         t &= 24
-        self.assertEqual(t, 24)
+        assert(t, 24)
         t &= t2
-        self.assertEqual(t, 8)
+        assert(t, 8)
 
     def test_tclobj_math17(self):
         """exercise tohil.tclobj "inplace bitwise xor" math ops"""
         t = tohil.tclobj(31)
 
         t ^= 15
-        self.assertEqual(t, 16)
+        assert(t, 16)
         t ^= 17
-        self.assertEqual(t, 1)
+        assert(t, 1)
 
     def test_tclobj_math18(self):
         """exercise tohil.tclobj "true divide" math ops"""
         t = tohil.tclobj(66)
 
-        self.assertEqual(t / 6, 11.0)
-        self.assertEqual(726 / t, 11.0)
-        self.assertEqual(t / t, 1.0)
+        assert(t / 6, 11.0)
+        assert(726 / t, 11.0)
+        assert(t / t, 1.0)
 
     def test_tclobj_math19(self):
         """exercise tohil.tclobj "inplace true divide" math ops"""
         t = tohil.tclobj(66)
 
         t /= 2
-        self.assertEqual(t, 33.0)
+        assert(t, 33.0)
         j = tohil.tclobj(3)
         t /= j
-        self.assertEqual(t, 11.0)
+        assert(t, 11.0)
         j /= j
-        self.assertEqual(j, 1.0)
+        assert(j, 1.0)
 
     def test_tclobj_math20(self):
         """exercise tohil.tclobj "floor divide" math ops"""
@@ -239,17 +272,17 @@ class TestTclObj(unittest.TestCase):
         t10 = tohil.tclobj(10)
         tm7 = tohil.tclobj(-7)
 
-        self.assertEqual(t66 // t10, 66 // 10)
-        self.assertEqual(66 // t10, 66 // 10)
-        self.assertEqual(t66 // 10, 66 // 10)
+        assert(t66 // t10, 66 // 10)
+        assert(66 // t10, 66 // 10)
+        assert(t66 // 10, 66 // 10)
 
-        self.assertEqual(t66 // tm7, 66 // -7)
-        self.assertEqual(66 // tm7, 66 // -7)
-        self.assertEqual(t66 // -7, 66 // -7)
+        assert(t66 // tm7, 66 // -7)
+        assert(66 // tm7, 66 // -7)
+        assert(t66 // -7, 66 // -7)
 
-        self.assertEqual(tm7 // t10, -7 // 10)
-        self.assertEqual(-7 // t10, -7 // 10)
-        self.assertEqual(tm7 // 10, -7 // 10)
+        assert(tm7 // t10, -7 // 10)
+        assert(-7 // t10, -7 // 10)
+        assert(tm7 // 10, -7 // 10)
 
     def test_tclobj_math21(self):
         """exercise tohil.tclobj "inplace floor divide" math ops"""
@@ -260,49 +293,49 @@ class TestTclObj(unittest.TestCase):
 
         t = t66
         t //= 2
-        self.assertEqual(t, 66 // 2)
+        assert(t, 66 // 2)
         t //= r5
-        self.assertEqual(t, 33 // 5)
+        assert(t, 33 // 5)
         t = t66
         t //= tm7
-        self.assertEqual(t, 66 // -7)
+        assert(t, 66 // -7)
 
     def test_tclobj_math22(self):
         """exercise tohil.tclobj inplace remainder ops"""
         t = tohil.tclobj(5)
         t %= 7
-        self.assertEqual(t, 5 % 7)
+        assert(t, 5 % 7)
 
         t = tohil.tclobj(11)
         t %= 5
-        self.assertEqual(t, 11 % 5)
+        assert(t, 11 % 5)
 
         t = tohil.tclobj(77)
         t %= -7
-        self.assertEqual(t, 77 % -7)
+        assert(t, 77 % -7)
 
         t = tohil.tclobj(-77)
         t %= -7
-        self.assertEqual(t, -77 % -7)
+        assert(t, -77 % -7)
 
         t = tohil.tclobj(-77)
         t %= 7
-        self.assertEqual(t, -77 % 7)
+        assert(t, -77 % 7)
 
         t = tohil.tclobj(-77)
         tm7 = tohil.tclobj('-7.')
         t %= tm7
-        self.assertEqual(t, -77 % -7.)
+        assert(t, -77 % -7.)
 
         t = tohil.tclobj(-77.)
         tm7 = tohil.tclobj('-7')
         t %= tm7
-        self.assertEqual(t, -77. % -7)
+        assert(t, -77. % -7)
 
         t = tohil.tclobj(-77.)
         tm7 = tohil.tclobj(-7.)
         t %= tm7
-        self.assertEqual(t, -77. % -7.)
+        assert(t, -77. % -7.)
 
 
 if __name__ == "__main__":
