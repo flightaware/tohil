b8639769043506af4118f9c59bbce2c81066565a
Karl Lehenbauer <karl-gh@lehenbauer.com>
2021-04-30 07:46:16 -0500

    convert more tests to use hypothesis

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,60 +1,65 @@
+
+import hypothesis
+from hypothesis import given, assume, strategies as st
 import unittest
 
 import tohil
 
 
 class TestMethods(unittest.TestCase):
-    def test_convert1(self):
+    @given(st.integers(-2000000, 2000000))
+    def test_convert1(self, i):
         """exercise tohil.convert with no to= and with to=str"""
-        self.assertEqual(tohil.convert(10), "10")
-        self.assertEqual(tohil.convert(10, to=str), "10")
-        self.assertEqual(tohil.convert("10"), "10")
-        self.assertEqual(tohil.convert("10", to=str), "10")
+        assert(tohil.convert(i) == str(i))
+        assert(tohil.convert(i, to=str) == str(i))
+        assert(tohil.convert(str(i)) == str(i))
 
-    def test_convert2(self):
+    @given(st.integers(-2000000, 2000000))
+    def test_convert2(self, i):
         """exercise tohil.convert and to=int and to=float"""
-        self.assertEqual(tohil.convert("10", to=int), 10)
-        self.assertEqual(tohil.convert("10", to=float), 10.0)
+        assert(tohil.convert(i, to=int) == i)
+        assert(tohil.convert(i, to=float) == float(i))
 
     def test_convert3(self):
         """exercise tohil.convert to=bool"""
-        self.assertEqual(tohil.convert(True, to=bool), True)
-        self.assertEqual(tohil.convert("t", to=bool), True)
-        self.assertEqual(tohil.convert("1", to=bool), True)
-        self.assertEqual(tohil.convert(1, to=bool), True)
-        self.assertEqual(tohil.convert(False, to=bool), False)
-        self.assertEqual(tohil.convert("f", to=bool), False)
-        self.assertEqual(tohil.convert("0", to=bool), False)
-        self.assertEqual(tohil.convert(0, to=bool), False)
+        assert(tohil.convert(True, to=bool) == True)
+        assert(tohil.convert("t", to=bool) == True)
+        assert(tohil.convert("1", to=bool) == True)
+        assert(tohil.convert(1, to=bool) == True)
+        assert(tohil.convert(False, to=bool) == False)
+        assert(tohil.convert("f", to=bool) == False)
+        assert(tohil.convert("0", to=bool) == False)
+        assert(tohil.convert(0, to=bool) == False)
 
-    def test_convert4(self):
+    @given(st.lists(st.integers(-1000000000, 1000000000)))
+    def test_convert4(self, ilist):
         """exercise tohil.convert to=list"""
-        self.assertEqual(tohil.convert("1 2 3 4 5", to=list), ["1", "2", "3", "4", "5"])
+        assert(tohil.convert(ilist, to=list) == ilist)
 
     def test_convert5(self):
         """exercise tohil.convert and to=dict"""
-        self.assertEqual(
+        assert(
             tohil.convert("a 1 b 2 c 3 d 4", to=dict),
             {"a": "1", "b": "2", "c": "3", "d": "4"},
         )
 
     def test_convert6(self):
         """exercise tohil.convert and to=tuple"""
-        self.assertEqual(
+        assert(
             tohil.convert("a 1 b 2 c 3 d 4", to=tuple),
             ("a", "1", "b", "2", "c", "3", "d", "4"),
         )
 
     def test_convert7(self):
         """exercise tohil.convert and to=set"""
-        self.assertEqual(
+        assert(
             sorted(tohil.convert("1 2 3 4 5 6 6", to=set)),
             ["1", "2", "3", "4", "5", "6"],
         )
 
     def test_convert8(self):
         """exercise tohil.convert and to=tohil.tclobj"""
-        self.assertEqual(
+        assert(
             repr(tohil.convert("1 2 3", to=tohil.tclobj)), "<tohil.tclobj: '1 2 3'>"
         )
 
